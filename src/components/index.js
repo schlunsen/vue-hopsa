c0f2f5869599f2a7052ac1a7b367285ff95bcb95
Rasmus Schlünsen <rasmus@schlunsen.com>
2019-02-13 22:34:49 +0100

    Fix export. Bump version

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
-import Hopsa from './Hopsa.vue'
+import Hopsa from './components/Hopsa.vue'
 import CircleAnimation from './animations/Circle.js'
 import BoxAnimation from './animations/Box.js'
-
 
 export {
     Hopsa,
