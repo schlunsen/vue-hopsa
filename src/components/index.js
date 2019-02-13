0bd02968989649acb180fa65f136aa609d4a4bf0
Rasmus Schlünsen <rasmus@schlunsen.com>
2019-02-13 22:24:41 +0100

    Updated export of components. Bump version

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,10 @@
-import Vue from 'vue'
 import Hopsa from './Hopsa.vue'
-
 import CircleAnimation from './animations/Circle.js'
 import BoxAnimation from './animations/Box.js'
 
-const Components = {
+
+export {
     Hopsa,
     CircleAnimation,
     BoxAnimation
 }
-Object.keys(Components).forEach(name => {
-    Vue.component(name, Components[name])
-})
-
-export default Components
