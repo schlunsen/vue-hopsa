17e7d3acbc8810afede83bf0f10dcd9dd92aa859
Rasmus Schlünsen <rasmus@schlunsen.com>
2019-02-14 00:28:59 +0100

    Refactored animation classes

---
+++ src/animations/BaseAnimation.js
@@ -0,0 +1,12 @@
+class BaseAnimation {
+
+    constructor(hopsaInstance) {
+        this.hopsaInstance = hopsaInstance;
+        
+        this.init();
+    }
+
+    
+}
+
+export default BaseAnimation
